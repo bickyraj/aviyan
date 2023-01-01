b5dbf6af687b14b20615d8ca728b9d113557528e
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2023-01-01 19:27:16 +0100

    updates on user authentication

--- resources/views/admin/elements/header.blade.php
+++ resources/views/admin/elements/header.blade.php
@@ -119,7 +119,13 @@
             <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
                 <div class="kt-header__topbar-user">
                     <span class="kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
-                    <span class="kt-header__topbar-username kt-hidden-mobile">Admin</span>
+                    <span class="kt-header__topbar-username kt-hidden-mobile">
+                        <?php if(auth()->user()->isAdmin()):?>
+                            Admin
+                        <?php else:?>
+                            {{ ucfirst(auth()->user()->name) }}
+                        <?php endif;?>
+                    </span>
                     <img class="kt-hidden" alt="Pic" src="./assets/media/users/300_25.jpg" />
                     <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
                     <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">A</span>
